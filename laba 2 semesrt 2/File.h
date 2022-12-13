aa15c7e2632f81ac548fcbebe446aa9c47ef5093
Pocket_brain <89377462+PocketBrain@users.noreply.github.com>
2022-12-13 23:46:53 +0300

    Add files via upload

---
+++ laba 2 semesrt 2/File.h
@@ -0,0 +1,18 @@
+
+#include "Keeper.h"
+#include <iostream>
+#include <fstream>
+#include <conio.h>
+#include <string>
+#include <cstring>
+
+
+using namespace std;
+
+void file()
+{
+    setlocale(LC_ALL, "rus");
+    
+    system("pause");
+
+}
